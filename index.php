eb3dbc594308c3c7e1c628c5b9a4479e80727f62
Cherry-eclair <98733660+Cherry-eclair@users.noreply.github.com>
2022-02-08 12:53:22 +0300

    тест

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ include_once "includes/functions.php";
 $posts = get_posts();
 $title = "Главная страница";
 $error = get_error_message();
-
+echo "test";
 include_once "includes/header.php";
 include_once "includes/tweet_form.php";
 include_once "includes/posts.php";
