09dc8e5e0c06ea04224e78015e5d0bbb8802c7ff
pkaswork <58027173+pkaswork@users.noreply.github.com>
2022-02-08 12:41:09 +0300

    Настройка рабочего места

--- includes/config.php
+++ includes/config.php
@@ -4,7 +4,7 @@ define('SITE_NAME', 'Twitter');
 define('HOST', 'http://'. $_SERVER['HTTP_HOST']);
 define('DB_HOST', $_SERVER['HTTP_HOST']);
 define('DB_NAME', 'twitter');
-define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_USER', 'twitter');
+define('DB_PASS', '123123');
 session_start();
 ?>
